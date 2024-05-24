6130f8017eb4b26817aec38ec8496430a84c89b7
Ben Vilakazi <xolanithegeengubane@gmail.com>
2024-05-24 15:54:18 +0200

    stage 1

---
+++ store-admin/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <p>Hello Admin Dashboard</p>
+      <Button size='default' variant='outline'> Click Me!</Button>
+
+    </div>
+  );
+}
